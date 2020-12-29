0cac65aaee76229b896590de718e0135627c4a8d
Reroet <wayanpuguhsudarma@gmail.com>
2020-12-29 13:46:42 +0800

    update with best practice

--- index.js
+++ index.js
@@ -4,8 +4,8 @@
  */
 
 // 1. Array.includes, Array.every, Array.some for multiple criteria
-export function validatingForm(name: string, username: string, birthday: string) {
-    if (name === '' || username === '' || birthday === '') {
+export function validatingForm(name: string, username: string, birthday: string, gender: string) {
+    if ([name, username, birthday, gender].includes('')) {
         console.log('Please complete the form');
     } else {
         // form is complete
@@ -19,14 +19,9 @@ export function ValidatingAllFruits() {
         { name: 'grape', color: 'purple' },
     ];
 
-    let isAllRed = true;
-
     // condition: all fruits must be red
-    for (const f of fruits) {
-        if (!isAllRed) break;
-
-        isAllRed = f.color === 'red';
-    }
+    const isAllRed = fruits.every((fruit) => fruit.color === 'red'); // false
+    fruits.some((fruit) => fruit.color === 'red'); // true
 
     console.log(isAllRed); // false
 }
@@ -35,54 +30,42 @@ export function ValidatingAllFruits() {
 export function pickMostBestFruit(fruit?: string, quantity?: number) {
     const redFruits = ['apple', 'strawberry', 'cherry', 'cranberries'];
 
-    // condition 1: fruit must has value
-    if (fruit) {
-        // condition 2: must be red
-        if (redFruits.includes(fruit)) {
-            console.log('red');
-
-            // condition 3: must be big quantity
-            if (quantity && quantity > 10) {
-                console.log('big quantity');
-            }
-        }
-    } else {
+    // check error first on fruits available
+    if (!fruit) {
         throw new Error('No fruit!');
+    }
+
+    // check all fruits not in array
+    if (!redFruits.includes(fruit)) {
+        return;
+    }
+
+    // print red
+    console.log('red');
+
+    // check quantity
+    if (quantity && quantity > 10) {
+        console.log('big quantity');
     }
 }
 
 // 3. Lookup map
 export function getStatusColor(status: string) {
-    let color = '';
+    const objColor = {
+        pending: 'red',
+        created: 'green',
+        progress: 'blue',
+        delivered: 'purple',
+        completed: 'torquise',
+        history: 'black',
+    };
 
-    if (status === 'pending') {
-        color = 'red';
-    } else if (status === 'created') {
-        color = 'green';
-    } else if (status === 'progress') {
-        color = 'blue';
-    } else if (status === 'delivered') {
-        color = 'purple';
-    } else if (status === 'completed') {
-        color = 'torquise';
-    } else {
-        color = 'yellow';
-    }
-
-    return color;
+    return objColor[status] || 'yellow';
 }
 
 // 4. Ternary operators
 export function getBestTitle(big: boolean) {
-    let title = '';
-
-    if (big) {
-        title = 'Big title';
-    } else {
-        title = 'Small title';
-    }
-
-    return title;
+    return big ? 'Big title' : 'Small title';
 }
 
 // 5. Short circuit Conditional (&&, ||)
@@ -111,6 +94,25 @@ const obj = {
 };
 
 // 1. Use map, foreach, find, filter, reduce to loop array
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+}
+
+arr.forEach((ar) => {
+    console.log(ar);
+});
+
+arr.find((x) => {
+    return x.name === 'apple';
+});
+
+arr.filter((x) => {
+    return x.name === 'apple';
+});
+
+arr.reduce((acc, item) => {
+    return [...acc, item.name];
+}, []); // ['apple', 'orange'];
 
 // 2. for iteration, For in and for of
 
@@ -120,40 +122,49 @@ const obj = {
  */
 
 // 1. Make it understandable
-function x() {
+export function validatingForm2() {
     //
 }
 
 // 2. Modularize, one function only take one responsibility
-export function userRegistration({ name }: any) {
+export async function userRegistration({ name }: any) {
+    try {
+        validatingUser(name);
+
+        const registerData = await registratingUser();
+        const loginData = await login();
+        const profileData = await profileData();
+
+        return { registerData, loginData, profileData };
+    } catch (error) {
+        // handle error
+    }
+}
+
+function validatingUser(name: string) {
     // validation
     if (name === '') {
         throw new Error('Name is required');
     }
+}
 
-    // registrating user
-    fetch('https://localhost/registration')
-        .then((response) => {
-            return response.json();
-        })
-        .then((data) => {
-            // login
-            return fetch('https://localhost/login');
-        })
-        .then((response) => {
-            return response.json();
-        })
-        .then((data) => {
-            // fetch profile
-            return fetch('https://localhost/profile');
-        })
-        .then((response) => {
-            return response.json();
-        })
-        .then((data) => {
-            return data;
-        })
-        .catch((error) => {
-            // handle error
-        });
+async function registratingUser() {
+    const response = await fetch('https://localhost/registration');
+    const data = await response.json();
+
+    return data;
+}
+
+async function login() {
+    const response = await fetch('https://localhost/login');
+    const data = await response.json();
+
+    return data;
+}
+
+export async function getProfile() {
+    const response = await fetch('https://localhost/profile');
+    const data = await response.json();
+
+    return data;
 }
